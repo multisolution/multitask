a4ebd42511d4ba738663b39ee32da0288c0f035f
leocavalcante <lc@leocavalcante.com>
2019-10-17 15:59:51 -0300

    Add web project setup

---
+++ web/pages/index.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+type IndexProps = {};
+
+const Index: FunctionComponent = (props: IndexProps) => {
+  return <p>multiTASK</p>;
+};
+
+export default Index;
